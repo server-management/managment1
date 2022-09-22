fea1dfb7df01e40c6a552a69f966f3bd7db9b245
srividyabhoopal <srividya@infobellit.com>
2022-09-22 15:27:45 +0530

    first push

---
+++ connect2DB.py
@@ -0,0 +1,21 @@
+import psycopg2
+#macking connection of database
+def connectDB():
+    filename = "db.config.py"
+    contents = open(filename).read()
+    config = eval(contents)
+    try:
+        dbase = psycopg2.connect(
+            host=config['host'],
+            dbname=config['dbname'],
+            user=config['user'],
+            password=config['password'],
+            port=config['port']
+        )
+        return dbase
+
+    except Exception as error:
+        print('check database details',error)
+
+if __name__ == "__main__":
+    connectDB()
